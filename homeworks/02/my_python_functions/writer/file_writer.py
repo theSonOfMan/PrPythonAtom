5c8cff8d1ef1b53eb85aaa1f8330907fca11d7f5
theSonOfMan <petr.galaburda@gmail.com>
2018-10-14 00:51:29 +0300

    FileWriter class

--- homeworks/02/my_python_functions/writer/file_writer.py
+++ homeworks/02/my_python_functions/writer/file_writer.py
@@ -6,21 +6,51 @@ class FileWriter:
     def __init__(self, path):
         """path - путь до файла"""
         if self._check_path(path):
+            print('valid path to file')
             self.path_ = path
             self.file = None
+        else:
+            print('path not valid')
         
     def _check_path(self, path):
-        pass
-    
+        if os.path.isfile(path):
+            return True
+        if os.path.isdir(path[:path.rfind('/')]):
+            return True
+        return False
+
+    def __enter__(self):
+        self.file = open(self.path_, 'a')
+        return self.file
+
+    def __exit__(self, type, value, traceback):
+        self.file.close()
+        self.file = None
+        return True        
+
     def print_file(self):
         pass
     
-    def write(self, some_string):
-        pass
+    @property
+    def path(self):
+        return self.path_
+
+    @path.setter
+    def path(self, new_path):
+        if self._check_path(new_path):
+            self.path_ = new_path
+        return None
+    
+    # def write(self, some_string):
+    #     self.file.write(some_string)
     
     def save_yourself(self, file_name):
-        pass
+        data = {'path': self.path_}
+        with open(file_name, 'wb') as file:
+            pkl.dump(data, file)
     
     @classmethod
     def load_file_writer(cls, pickle_file):
-        pass
+        with open(pickle_file, 'rb') as file:
+            data = pkl.load(file)
+        return FileWriter(data['path'])
